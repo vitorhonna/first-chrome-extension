7e459e850da37067d6d2e062d2f334175db3024b
Vitor Honna <vitorhonna@gmail.com>
2021-11-19 15:28:13 -0300

    Fully functional extension

---
+++ popup.js
@@ -0,0 +1,34 @@
+// Initialize button with user's preferred color
+let changeColor = document.getElementById("changeColor");
+
+chrome.storage.sync.get("color", ({ color }) => {
+    changeColor.style.backgroundColor = color;
+});
+
+changeColor.addEventListener("click", (e) => {
+    "";
+    console.log("clicked");
+    console.log(e);
+});
+
+// When the button is clicked, inject setPageBackgroundColor into current page
+changeColor.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    let all = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    console.log(all);
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: setPageBackgroundColor,
+    });
+});
+
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+    chrome.storage.sync.get("color", ({ color }) => {
+        document.body.style.backgroundColor = color;
+    });
+}
